f1a51d3f51e6fa67b9967a4020ef64b2fe748634
José David Melián Villalobos <josedavidmelian@gmail.com>
2021-07-06 15:52:51 +0100

    First commmit, adding resources

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+//import { AuthService  } from "../usuarios/auth.service";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+    title: string = 'App Angular'
+
+    constructor(private router: Router){
+
+    }
+
+    // logout(): void{
+    //     Swal.fire("Has Cerrado Sesión", `Adios ${this.authService.usuario.username}`, "success");
+    //     this.authService.logout();        
+    //     this.router.navigate(['/login']);
+    // }
+ 
+}
+
